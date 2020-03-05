ab70eef935d4e2d439babb732b4f5870428875bb
Danny Hart <danny.hart@jointhedotsmr.com>
2020-03-05 11:24:44 +0000

    Initial commit

---
+++ src/components/SayHello.vue
@@ -0,0 +1,25 @@
+<template>
+  <button 
+    type="button" 
+    @click="sayHello"
+  >
+    Say Hello
+  </button>
+</template>
+
+<script>
+export default {
+  name: 'SayHello',
+  props: {
+    name: {
+      type: String,
+      default: 'World'
+    }
+  },
+  methods: {
+    sayHello() {
+      alert(`Hello, ${this.name}!`);
+    }
+  }
+}
+</script>
